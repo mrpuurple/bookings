bfd546a62fe86ccd2a6aaa0ea7e2061545ee6a6a
Nichlas Handstedt <nichlas.handstedt@scania.com>
2023-07-25 12:26:12 +0200

    add AJAX

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,8 +21,11 @@ func routes(a *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
+
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
+	
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static"))
