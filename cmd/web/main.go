4d1c3d889aa6b372cb1094b2b835850f1d758947
Nichlas Handstedt <nichlas.handstedt@scania.com>
2023-08-09 12:59:59 +0200

    add database repository pattern

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/mrpuurple/bookings/internal/config"
+	"github.com/mrpuurple/bookings/internal/driver"
 	"github.com/mrpuurple/bookings/internal/handlers"
 	"github.com/mrpuurple/bookings/internal/helpers"
 	"github.com/mrpuurple/bookings/internal/models"
@@ -24,10 +25,11 @@ var errorLog *log.Logger
 
 // main is the main application function
 func main() {
-	err := run()
+	db, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.SQL.Close()
 
 	fmt.Printf("Starting application on port %s\n", portNumber)
 
@@ -41,7 +43,7 @@ func main() {
 
 }
 
-func run() error {
+func run() (*driver.DB, error) {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 	// change this to true when in production
@@ -62,20 +64,28 @@ func run() error {
 
 	app.Session = session
 
+	// Connect to database
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings user=testuser password=test1234!")
+	if err != nil {
+		log.Fatal("Cannot connect to database! Dying...")
+	}
+	log.Println("Connected to database!")
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
-		return err
+		return nil, err
 	}
 	
 	app.TemplateCache = tc
 	app.UseCache = true
 
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 	helpers.NewHelpers(&app)
 
-	return nil
+	return db, nil
 }
  
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,10 +6,13 @@ import (
 	"net/http"
 
 	"github.com/mrpuurple/bookings/internal/config"
+	"github.com/mrpuurple/bookings/internal/driver"
 	"github.com/mrpuurple/bookings/internal/forms"
 	"github.com/mrpuurple/bookings/internal/helpers"
 	"github.com/mrpuurple/bookings/internal/models"
 	"github.com/mrpuurple/bookings/internal/render"
+	"github.com/mrpuurple/bookings/internal/repository"
+	"github.com/mrpuurple/bookings/internal/repository/dbrepo"
 )
 
 // Repo the repository used by the handlers
@@ -18,12 +21,14 @@ var Repo *Repository
 // Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
+	DB repository.DatabaseRepo
 }
 
 // NewRepo creates a new repository
-func NewRepo(a *config.AppConfig) *Repository {
+func NewRepo(a *config.AppConfig, db *driver.DB) *Repository {
 	return &Repository{
 		App: a,
+		DB: dbrepo.NewPostgresRepo(db.SQL, a),
 	}
 }
 
