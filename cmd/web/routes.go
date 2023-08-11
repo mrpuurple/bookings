10b8bb29c0f4145e2c0849ec81f83190b9af5610
Nichlas Handstedt <nichlas.handstedt@scania.com>
2023-08-11 16:01:17 +0200

    generals quarters room working

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,14 +18,16 @@ func routes(a *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-	mux.Get("/contact", handlers.Repo.Contact)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
-
+	
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 	mux.Get("/choose-room/{id}", handlers.Repo.ChooseRoom)
+	mux.Get("/book-room", handlers.Repo.BookRoom)
+
+	mux.Get("/contact", handlers.Repo.Contact)
 	
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
