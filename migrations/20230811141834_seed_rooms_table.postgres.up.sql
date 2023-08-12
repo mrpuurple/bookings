d022698a11b91acac5aa9db550c2df329135728e
Nichlas Handstedt <nichlas.handstedt@scania.com>
2023-08-12 08:45:18 +0200

    add seed data migrations

---
+++ migrations/20230811141834_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (id,room_name,created_at,updated_at) VALUES
+(1,'General''s Quarters','2023-08-01 00:00:00','2023-08-01 00:00:00'),
+(2,'Major''s Suite','2023-08-01 00:00:00','2023-08-01 00:00:00');
---
+++ migrations/20230811144455_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('Reservation','2023-08-10 00:00:00','2023-08-10 00:00:00'),
+('Owner Block','2023-08-11 00:00:00','2023-08-11 00:00:00');
